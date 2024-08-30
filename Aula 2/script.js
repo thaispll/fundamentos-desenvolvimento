838254c889a3cfb653855bbca9ef25f57b975689
Thais Leal <58890658+thaispll@users.noreply.github.com>
2024-08-29 21:36:54 -0300

    Add files via upload

---
+++ Aula 2/script.js
@@ -0,0 +1,7 @@
+console.log("Olá, Mundo!");
+let preco = prompt("Digite um preço:");
+document.write("O preço é:",preco, "<br>");
+
+let nome = prompt("Digite o seu nome:");
+document.write("Olá " ,nome,". Seja bem-vindx ao site! ");
+//olá, fulana. Seja bem-vindx ao site!
